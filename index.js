2a51bc47325c27273cb9a517c102c6339cd36b40
jiten2032 <jiten2032@gmail.com>
2021-01-10 01:58:43 +0530

    GitHub_Finder

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-//Client secrets = 78eb382a710f335e8b0db0bade6b09b861839be5 ;
-// Client ID = b054f411b14d9231c21d ;
 
 let input = document.getElementById("InputBox");
 input.addEventListener('keyup', UserName);
 function UserName() {
-let TextEntered =  input.value ;
-let Output = document.getElementById("output");
-Output.innerHTML  = TextEntered ;
+    let TextEntered = input.value;
+
+    //creat a new github object 
+    let Github = new GithubFinder
+
+    //call the github function 
+    Github.github(TextEntered).then(function (data) {
+        console.log(data)
+    })
+    .catch(function(error){
+        console.log(error)
+    })
+
+
+
+    // let Output = document.getElementById("output");
+    // Output.innerHTML  = TextEntered ;
+
+
+
 
 
 }
