80a8b7b360be9ed47da5f10605404b1dd4deb97c
jiten2032 <jiten2032@gmail.com>
2021-01-13 00:00:17 +0530

    GitHub Finder

--- index.js
+++ index.js
@@ -5,55 +5,65 @@ function UserName() {
     let TextEntered = input.value;
 
     //creat a new github object 
-    let Github = new GithubFinder
 
-    //call the github function 
-    Github.github(TextEntered).then(function (data) {
-
-        // console.log(data)
-        let Output = document.getElementById("output");
-        Output.innerHTML = `
-    <div class="d-flex justify-content-evenly flex-wrap">
-        <div class="">
-             <img src="${data.avatar_url}" class="img-fluid d-block " >
-
-           <a href="${data.html_url}" target="_blank" class="btn btn-primary btn-lg btn-block mt-3 mb-3 ">
-           View Profile
-           </a>
-        </div>
-        <div class="">
-                        <span class="badge  bg-primary p-2">Public_Repos : ${data.public_repos}</span>
-                        <span class="badge bg-secondary p-2">Public_Gists : ${data.public_gists}</span>
-                        <span class="badge bg-success p-2">Followers : ${data.followers}</span>
-                        <span class="badge bg-danger p-2">Following : ${data.following}</span>    
-                        
-                        <div class="card mt-3" style="width: 28rem;">
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item ">Name : ${data.name}</li>
-                                    <li class="list-group-item ">Location : ${data.location}</li>
-                                    <li class="list-group-item ">Comapny : ${data.company}</li>
-                                    <li class="list-group-item ">Website/Blog : ${data.blog}</li>
-                                    <li class="list-group-item ">Member Since : ${data.created_at}</li>
-                                    <li class="list-group-item ">Last Updated In : ${data.updated_at}</li>
-                                </ul>
-                         </div>
+    if (TextEntered != '') {
+        let Github = new GithubFinder
+        Github.github(TextEntered).then(function (data) {
+            if (`${data.message}` == "Not Found") {
+                document.getElementById("output").innerHTML = `
+                         <div class="container alert alert-danger mt-3 text-center" role="alert">
+                        <h4 class="alert-heading">Oh Sorry!</h4>
+                        <p>Entered Username not Available</p>
+                        <hr>
+                        <p class="mb-0">Please Enter a Valid Username.</p>
+                        </div> `
+                } 
+                else {
 
 
-        </div>
-    </div>
-       `
-    })
-    .catch(function(error){
-        console.log(error)
-    })
+                let Output = document.getElementById("output");
+                Output.innerHTML = `
+                  <div class="container d-flex justify-content-evenly flex-wrap mt-5 ">
+                    <div class="mt-3 ms-2">
+                 <img src="${data.avatar_url}" class="img-fluid d-block " >
+    
+                 <a href="${data.html_url}" target="_blank" class="btn btn-primary btn-lg btn-block mx-auto d-block mt-3 mb-3 ">
+                     View Profile
+                    </a>
+                   </div>
+                  <div class="mt-3 ms-2">
+                            <span class="badge  bg-primary p-2">Public_Repos : ${data.public_repos}</span>
+                            <span class="badge bg-secondary p-2">Public_Gists : ${data.public_gists}</span>
+                            <span class="badge bg-success p-2">Followers : ${data.followers}</span>
+                            <span class="badge bg-danger p-2">Following : ${data.following}</span>    
+                            
+                            <div class="card mt-3" style="width: 28rem;">
+                                    <ul class="list-group list-group-flush">
+                                        <li class="list-group-item ">Name : ${data.name}</li>
+                                        <li class="list-group-item ">Location : ${data.location}</li>
+                                        <li class="list-group-item ">Comapny : ${data.company}</li>
+                                        <li class="list-group-item ">Website/Blog : ${data.blog}</li>
+                                        <li class="list-group-item ">Member Since : ${data.created_at}</li>
+                                        <li class="list-group-item ">Last Updated In : ${data.updated_at}</li>
+                                    </ul>
+                             </div>
+    
+    
+                      </div>
+                    </div>
+                          `
+            }
+            // console.log(data)
+            
+        })
+            .catch(function (error) {
+                document.getElementById("output").innerHTML = `<h1 class="text-center" >${error}</h1>`
+            })
 
+    }
+    else {
 
-
-    // let Output = document.getElementById("output");
-    // Output.innerHTML  = TextEntered ;
-
-
-
-
+        document.getElementById("output").innerHTML = "";
+    }
 
 }
